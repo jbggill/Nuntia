fd1888f1e902cec26fd420bb1ca42c36eaa19007
Jesse Gill <138601900+jbggill@users.noreply.github.com>
2023-07-14 17:18:40 +0100

    Update BE-test.py
    Created Tests for the backend

--- BackEnd/BE-test.py
+++ BackEnd/BE-test.py
@@ -1,5 +1,41 @@
-import political_bias
-import article_processor
-contents = article_processor.getContents('https://www.foxnews.com/media/democratic-strategist-scolds-biden-not-recognizing-seventh-grandchild-humanity')
-bias = political_bias.BERT(contents[:len(contents)//2])
-print(bias)
+import unittest
+from app import app
+
+
+class AppTests(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_handle_post_with_html(self):
+        # Test the POST request with HTML content
+        with open('example_article.txt','r') as txt:
+            html = txt.read()
+        data = {'html': html}
+        response = self.app.post('/', json=data)
+        data = response.get_json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('message', data)
+        self.assertIsInstance(data['message'], dict)
+
+    def test_handle_post_with_url(self):
+        # Test the POST request with URL
+        data = {'url': 'https://edition.cnn.com/europe/live-news/russia-ukraine-war-news-07-14-23/index.html'}
+        response = self.app.post('/', json=data)
+        data = response.get_json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('message', data)
+        self.assertIsInstance(data['message'], dict)
+
+    def test_handle_post_insufficient_information(self):
+        # Test the POST request with insufficient information
+        data = {}
+        response = self.app.post('/', json=data)
+        data = response.get_json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('error', data)
+
+if __name__ == '__main__':
+    unittest.main()
