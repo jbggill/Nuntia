6ca6528bacd033f89f783ef47c00b53be54488ab
Jesse Gill <138601900+jbggill@users.noreply.github.com>
2023-07-18 18:58:59 +0100

    added sliders

--- FrontEnd/popup/popup.js
+++ FrontEnd/popup/popup.js
@@ -22,21 +22,46 @@ document.getElementById('read-content').addEventListener('click', () => {
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action == "setBiasScore"){
-     const data = request.data.message
-     if (data != null && data != ''){
-      sendResponse('data recieved')
-      console.log(data)
-      console.log(data.left)
-      const biasScoreElementLeft = document.getElementById('page-bias-value-left');
+    const data = request.data.message;
+    if (data != null && data != ''){
+      sendResponse('data received');
+      console.log(data);
+      
+      const biasScoreElementLeft = document.getElementById('bias-score-left');
       biasScoreElementLeft.textContent = `Left: ${data.left}%`;
 
-      const biasScoreElementRight = document.getElementById('page-bias-value-right');
+      const biasScoreElementRight = document.getElementById('bias-score-right');
       biasScoreElementRight.textContent = `Right: ${data.right}%`;
 
-      const biasScoreElementCentre = document.getElementById('page-bias-value-center');
+      const biasScoreElementCentre = document.getElementById('bias-score-center');
       biasScoreElementCentre.textContent = `Center: ${data.center}%`;
-     }
+      
+      // Set the slider values based on the received data
+      $('#bias-slider-left').val(data.left);
+      $('#bias-slider-right').val(data.right);
+      $('#bias-slider-center').val(data.center);
     }
-})
+  }
+});
 
+// Handle slider input events to update the bias percentages
+/** 
+$('.slider-container input[type="range"]').on('input', function() {
+  const sliderId = this.id;
+  const sliderValue = this.value;
+  const biasScoreElement = document.getElementById(`bias-score-${sliderId.replace('bias-slider-', '')}`);
+  biasScoreElement.textContent = `${sliderId.replace('bias-slider-', '').replace('-', ' ')}: ${sliderValue}%`;
 
+  // Send updated slider values to the background script
+  chrome.runtime.sendMessage({
+    action: 'updateBiasScore',
+    data: {
+      left: $('#bias-slider-left').val(),
+      right: $('#bias-slider-right').val(),
+      center: $('#bias-slider-center').val(),
+    }
+  }, (response) => {
+    console.log(response);
+  });
+});
+*/
